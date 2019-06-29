a74d1476b1269df5368af41ee37037baf2d91d58
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-29 12:52:32 +0800

    fix test

--- src/test/java/idv/kuma/ImmutableQueueTest.java
+++ src/test/java/idv/kuma/ImmutableQueueTest.java
@@ -36,10 +36,15 @@ public class ImmutableQueueTest {
 
         Queue<Integer> before = makeQueue();
 
-        Queue<Integer> after = before.enQueue(1);
-        Assert.assertNotEquals(before, after);
-        Assert.assertEquals(Integer.valueOf(1), after.head());
+        Queue<Integer> afterEnqueue = before.enQueue(1);
+        Assert.assertNotEquals(before, afterEnqueue);
+        Assert.assertEquals(Integer.valueOf(1), afterEnqueue.head());
+
+
     }
+
+
+
 
     private Queue<Integer> makeQueue(int... elements) {
 
@@ -50,17 +55,6 @@ public class ImmutableQueueTest {
         }
 
         return queue;
-    }
-
-
-    @Test
-    public void Given_Queue_1_When_Enqueue_2_Then_Return_New_Queue_And_Head_2() {
-
-        Queue<Integer> before = makeQueue(1);
-
-        Queue<Integer> after = before.enQueue(2);
-        Assert.assertNotEquals(before, after);
-        Assert.assertEquals(Integer.valueOf(1), after.head());
     }
 
 
