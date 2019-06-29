1693e116b43b4f539b0f16a27a290ba61d7f9a33
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-29 12:42:44 +0800

    when calling head to empty queue, then throw exception

--- src/test/java/idv/kuma/ImmutableQueueTest.java
+++ src/test/java/idv/kuma/ImmutableQueueTest.java
@@ -3,7 +3,23 @@ package idv.kuma;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 public class ImmutableQueueTest {
+
+    @Test
+    public void Given_Queue_Empty_When_Head_Then_Exception() {
+
+        Queue<Integer> queue = new ImmutableQueue<>();
+        
+        try {
+            queue.head();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof NoSuchElementException);
+        }
+
+    }
+
 
     @Test
     public void Given_Queue_Empty_When_Enqueue_1_Then_Return_New_Queue_And_Head_1() {
