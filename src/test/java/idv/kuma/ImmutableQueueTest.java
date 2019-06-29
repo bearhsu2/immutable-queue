78121f5ef0f2e62e90a40167fb41b8afc90b26c6
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-29 12:36:56 +0800

    1 enqueue

--- src/test/java/idv/kuma/ImmutableQueueTest.java
+++ src/test/java/idv/kuma/ImmutableQueueTest.java
@@ -16,4 +16,16 @@ public class ImmutableQueueTest {
     }
 
 
+    @Test
+    public void Given_Queue_1_When_Enqueue_2_Then_Return_New_Queue_And_Head_2() {
+
+        Queue<Integer> before = new ImmutableQueue<>();
+        before.enQueue(1);
+
+        Queue<Integer> after = before.enQueue(2);
+        Assert.assertNotEquals(before, after);
+        Assert.assertEquals(Integer.valueOf(2), after.head());
+    }
+
+
 }
